ea382ebd96ebc1a77b732078e21299b2235899f2
rizky-putra19 <rizkyagrasyahputra19@gmail.com>
2021-11-27 13:42:04 +0700

    deploment

--- middlewares/uploadPhotoProduct.js
+++ middlewares/uploadPhotoProduct.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
+    api_key: process.env.CLOUD_API_KEY,
     api_secret: process.env.API_SECRET,
 });
 
